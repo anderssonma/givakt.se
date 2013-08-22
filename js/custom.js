eedf9db419f205a4d1b793b8b9ea9c53cdbb338c
mikael <mikael.hjelm@gmail.com>
2013-08-22 16:19:52 +0200

    some small chages
    kontakt, om-oss

--- js/custom.js
+++ js/custom.js
@@ -3,9 +3,5 @@ $(document).ready(function(){
 });
 
 $(window).load(function(){
-  var blogg_image_height = $(".blogg-top-img").height();
-  	$(".blogg-top-img").css("height", blogg_image_height);
 
-  	var about_us = $(".about-us-top").height();
-  	$(".about-us-top").css("height", about_us);
 });
