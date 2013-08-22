1329ac5c3454d2cdfe57798957cd8035df55a309
Marcus Andersson <marcus@marcus-andersson.com>
2013-08-22 14:48:18 +0200

    Small refactor

--- js/custom.js
+++ js/custom.js
@@ -2,10 +2,7 @@ $(document).ready(function(){
 
 });
 
-$(window).load(function(){
-  var blogg_image_height = $(".blogg-top-img").height();
-  	$(".blogg-top-img").css("height", blogg_image_height);
-
-  	var about_us = $(".about-us-top").height();
-  	$(".about-us-top").css("height", about_us);
+$(window).load(function() {
+	$(".blogg-top-img").css("height", $(".blogg-top-img").height());
+	$(".about-us-top").css("height", $(".about-us-top").height());
 });
