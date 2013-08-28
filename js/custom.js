d61bc1e3dedf1f78de617a7303da5f18de70ace5
Marcus Andersson <marcus@marcus-andersson.com>
2013-08-28 09:06:21 +0200

    Menu cleanup, 'pure css'

--- js/custom.js
+++ js/custom.js
@@ -1,40 +1,19 @@
-var animEndEventNames = {
-    'WebkitAnimation' : 'webkitAnimationEnd',
-    'OAnimation' : 'oAnimationEnd',
-    'msAnimation' : 'MSAnimationEnd',
-    'animation' : 'animationend'
-};
 
 $(document).ready(function(){
 
-  var isOpen = false;
-  var animEnd = animEndEventNames[Modernizr.prefixed('animation')];
-
-    /* IS IT POSSIBLE TO USE TRANSLATEX() WITH A FIXED ELEMENT */
-    /* AKA - USE CSS INSTEAD */
-  
-      $('#nav-open-btn').on('click', function(e) {
-        if (!isOpen) {
-            $(this).css('-webkit-transform', 'translateX(-250px)');
-            $('#inner-wrap').addClass('menu-open');
-            $('nav').addClass('show');
-            isOpen = true;
-        } else {
-            $(this).css('-webkit-transform', 'translateX(0)');
-            $('#inner-wrap').removeClass('menu-open');
-            $('nav').removeClass('show');
-            isOpen = false;
-        }
-      });
-
+  $('#nav-open-btn').on('click', function() {
+    $(this).toggleClass('menu-open');
+    $('#inner-wrap').toggleClass('menu-open');
+    $('nav').toggleClass('show');
+  });
 
   $('#nav-close-btn').on('click', function() {
-    $('#nav-open-btn').css('-webkit-transform', 'translateX(0)');
+    $('#nav-open-btn').removeClass('menu-open');
     $('#nav-open-btn, #inner-wrap').removeClass('menu-open');
     $('nav').removeClass('show');
   });
-});
 
+});
 
 $(window).load(function() {
 
